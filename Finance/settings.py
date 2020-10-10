096dcac100493dc16c9eb690bd4f9ac5aa8abe22
Apurv Tiwari <apurv.tiwari@upstart.com>
2020-10-10 16:30:04 -0700

    updated code

--- Finance/settings.py
+++ Finance/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'prz5ydg*k_xdw@(y-7mqo2gbyvpfw2tq+dx2hbm2-fd6kl9e@g'
+SECRET_KEY = "prz5ydg*k_xdw@(y-7mqo2gbyvpfw2tq+dx2hbm2-fd6kl9e@g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,67 +31,59 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'data'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "data",
+    "yahoofinancials"
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'Finance.urls'
+ROOT_URLCONF = "Finance.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'Finance.wsgi.application'
+WSGI_APPLICATION = "Finance.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'finance',
-
-        'USER': 'vader',
-
-        'PASSWORD': '79galois',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "finance",
+        "USER": "postgres",
+        "PASSWORD": "79galois",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
-
 }
 
 
@@ -100,26 +92,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -131,4 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
+TIINGO_API_KEY = '2b9cfdf172766065bcce1ce07353440f74fb40da'
--- data/models.py
+++ data/models.py
@@ -2,80 +2,51 @@ from django.db import models
 import pandas as pd
 from datetime import datetime
 import json
+from yahoofinancials import YahooFinancials
 
 # Create your models here.
 
 class Company(models.Model) :
     ticker = models.TextField(unique=True)
     name = models.TextField(blank=True, null=True)
+    
+    def refresh(self) :
+        yahoo_financials = YahooFinancials([self.ticker])
+        data = yahoo_financials.get_financial_stmts('annual', ['income', 'cash', 'balance'])
+        income_stm = json.dumps(data['incomeStatementHistory'][self.ticker])
+        balance_sheet = json.dumps(data['balanceSheetHistory'][self.ticker])
+        cashflow_stm = json.dumps(data['cashflowStatementHistory'][self.ticker])
+        
+        bs_data = BalanceSheet.objects.filter(company=self).order_by('-created_at')
+        if len(bs_data) == 0 or bs_data[0].data != balance_sheet:
+            bs = BalanceSheet(company=self, data=balance_sheet)
+            bs.save()
+        
+        is_data = IncomeStatement.objects.filter(company=self).order_by('-created_at')
+        if len(is_data) == 0 or is_data[0].data != income_stm:
+            iss = IncomeStatement(company=self, data=income_stm)
+            iss.save()
+        
+        cfs_data = CashFlowStatement.objects.filter(company=self).order_by('-created_at')
+        if len(cfs_data) == 0 or cfs_data[0].data != cashflow_stm:
+            cfs = CashFlowStatement(company=self, data=cashflow_stm)
+            cfs.save()
+        
 
 class BalanceSheet(models.Model) :
     company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
     data = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    _multiplier = 1e3
-    
-    @property
-    def df(self) :
-        return pd.read_json(self.data)
-    
-    @property
-    def dates(self) :
-        dates = []
-        for date in self.df.columns[1:] :
-            dates.append(datetime.strptime(date, '%m/%d/%Y'))
-        
-        return dates
-    
-    def _data_selector(self, column_name, date=None) :
-        if not date :
-            date = datetime.now()
-        
-        index = 0
-        for index in range(len(self.dates)) :
-            _date = self.dates[index]
-            if date > _date :
-                break
-        
-        return self.df[self.df['Breakdown'] == column_name].to_numpy()[0, index + 1] * self._multiplier
-    
-    def total_assets(self, date=None) :
-        column_name = 'Total Assets'
-        return self._data_selector(column_name, date)
-    
-    def net_tangible_assets(self, date=None) :
-        column_name = 'Net Tangible Assets'
-        return self._data_selector(column_name, date)
-    
-    def enterprise_value(self, date=None) :
-        column_name = 'Total Capitalization'
-        return self._data_selector(column_name, date)
-    
-    def net_debt(self, date=None) :
-        column_name = 'Net Debt'
-        return self._data_selector(column_name, date)
-    
-    def total_debt(self, date=None) :
-        column_name = 'Total Debt'
-        return self._data_selector(column_name, date)
 
-class QuoteTable(models.Model) :
+class IncomeStatement(models.Model) :
     company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
     data = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
-    @property
-    def _quote(self) :
-        return json.loads(self.data)
-    
-    def market_cap(x) :
-        m_cap = self._quote["Market Cap"]
-        if m_cap.endswith("T") :
-            return float(m_cap[:-1]) * 1e12
-        if m_cap.endswith("B") :
-            return float(m_cap[:-1]) * 1e9
-        if m_cap.endswith("M") :
-            return float(m_cap[:-1]) * 1e6
-        return float(m_cap)
+
+class CashFlowStatement(models.Model) :
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+    data = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
